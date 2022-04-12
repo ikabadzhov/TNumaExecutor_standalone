84477c969dc06beb8f215f80e7a0ca3f03fc7c08
Ivan Kabadzhov <kabadzhovivan@gmail.com>
2022-04-12 09:44:26 +0200

    Fix reproducer

--- test2.cxx
+++ test2.cxx
@@ -1,39 +1,40 @@
+#include "ROOT/RTaskArena.hxx"
+#include <ROOT/TProcessExecutor.hxx>
+#include <ROOT/TThreadExecutor.hxx>
+#include <ROOT/TNUMAExecutor.hxx>
+
 #include "ROOT/TSeq.hxx"
-#include "TNUMA_Alpha.hxx"
-#include "TStopwatch.h"
 #include <iostream>
 
-void numa() {
+#include "TStopwatch.h"
 
-  auto func = [&](long int) -> long int {
-    long int x{};
-    ROOT::TSeq<long int> s(100 * 1000 * 1000);
-    for (auto y : s)
+void numa() {
+  std::vector<long int> aux(200);
+  std::iota(aux.rbegin(), aux.rend(), aux.size());
+
+  auto func = [&](long int x) -> long int {
+    for (auto y : aux)
       x = (x * 1009) % 71 + y;
-    // non-trivial action to avoid cache optimizations
     return x;
   };
-
   auto redfunc = [](const std::vector<long int> &vec) {
     long int res = 0;
     for (size_t i = 0; i < vec.size(); ++i) {
-      res += 1009 * vec[i * 1009 % vec.size()] % (vec.size() + 1) + vec[i];
-      // non-trivial action to avoid cache optimizations
+      res += vec[i];
     }
     return res;
   };
 
-  std::vector<long int> v(120);
+  std::vector<long int> v(100 * 1000 * 1000);
+  std::iota(v.rbegin(), v.rend(), v.size());
 
-  ROOT::Experimental::TNUMAExecutor n;
-  ROOT::TThreadExecutor n;
-
+  //ROOT::Experimental::TNUMAExecutor n;
+  ROOT::TThreadExecutor t;
   TStopwatch StopWatch;
   StopWatch.Reset();
   StopWatch.Start();
-
-  std::cout << "numa -> " << n.MapReduce(func, v, redfunc) << std::endl;
-  //std::cout << "thrd -> " << t.MapReduce(func, v, redfunc) << std::endl;
+  //std::cout << "numa -> " << n.MapReduce(func, v, redfunc) << std::endl;
+  std::cout << "thrd -> " << t.MapReduce(func, v, redfunc, 10000) << std::endl;
   StopWatch.Stop();
   std::cout << StopWatch.RealTime() << std::endl;
 }
