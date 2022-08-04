8d93729d1f265031636304436f0291eb6d9e8f31
Ivan Donchev Kabadzhov <ikabadzh@olhsw-05.cern.ch>
2022-08-04 11:07:51 +0200

    Add odb 6 and 7

---
+++ rdf_benchmarks/odb6.cpp
@@ -0,0 +1,80 @@
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RVec.hxx"
+#include "TCanvas.h"
+#include "Math/Vector4D.h"
+
+#include <iostream>
+#include "TStopwatch.h"
+
+//#include <ROOT/RLogger.hxx>
+
+//auto verbosity = ROOT::Experimental::RLogScopedVerbosity(ROOT::Detail::RDF::RDFLogChannel(), ROOT::Experimental::ELogLevel::kInfo);
+
+template <typename T> using Vec = const ROOT::RVec<T>&;
+using ROOT::Math::XYZTVector;
+
+ROOT::RVec<std::size_t> find_trijet(Vec<XYZTVector> jets) {
+  const auto c = ROOT::VecOps::Combinations(jets, 3);
+
+  float distance = 1e9;
+  const auto top_mass = 172.5;
+  std::size_t idx = 0;
+  for (auto i = 0u; i < c[0].size(); i++) {
+    auto p1 = jets[c[0][i]];
+    auto p2 = jets[c[1][i]];
+    auto p3 = jets[c[2][i]];
+    const auto tmp_mass = (p1 + p2 + p3).mass();
+    const auto tmp_distance = std::abs(tmp_mass - top_mass);
+    if (tmp_distance < distance) {
+      distance = tmp_distance;
+      idx = i;
+    }
+  }
+
+  return {c[0][idx], c[1][idx], c[2][idx]};
+}
+
+
+float trijet_pt(Vec<float> pt, Vec<float> eta, Vec<float> phi, Vec<float> mass, Vec<std::size_t> idx)
+{
+    auto p1 = ROOT::Math::PtEtaPhiMVector(pt[idx[0]], eta[idx[0]], phi[idx[0]], mass[idx[0]]);
+    auto p2 = ROOT::Math::PtEtaPhiMVector(pt[idx[1]], eta[idx[1]], phi[idx[1]], mass[idx[1]]);
+    auto p3 = ROOT::Math::PtEtaPhiMVector(pt[idx[2]], eta[idx[2]], phi[idx[2]], mass[idx[2]]);
+    return (p1 + p2 + p3).pt();
+}
+
+
+void rdataframe() {
+    using ROOT::Math::PtEtaPhiMVector;
+    using ROOT::VecOps::Construct;
+
+    ROOT::EnableImplicitMT();
+    ROOT::RDataFrame df("Events", std::vector<std::string>(3, "../../Run2012B_SingleMu.root"));
+    auto df2 = df.Filter([](unsigned int n) { return n >= 3; }, {"nJet"}, "At least three jets")
+                 .Define("JetXYZT", [](Vec<float> pt, Vec<float> eta, Vec<float> phi, Vec<float> m) {
+                              return Construct<XYZTVector>(Construct<PtEtaPhiMVector>(pt, eta, phi, m));},
+                         {"Jet_pt", "Jet_eta", "Jet_phi", "Jet_mass"})
+                 .Define("Trijet_idx", find_trijet, {"JetXYZT"});
+    auto h1 = df2.Define("Trijet_pt", trijet_pt, {"Jet_pt", "Jet_eta", "Jet_phi", "Jet_mass", "Trijet_idx"})
+                 .Histo1D<float>({"", ";Trijet pt (GeV);N_{Events}", 100, 15, 40}, "Trijet_pt");
+    auto h2 = df2.Define("Trijet_leadingBtag",
+                         [](const ROOT::RVec<float> &btag, const ROOT::RVec<std::size_t> &idx) { return Max(Take(btag, idx)); },
+                         {"Jet_btag", "Trijet_idx"})
+                 .Histo1D<float>({"", ";Trijet leading b-tag;N_{Events}", 100, 0, 1}, "Trijet_leadingBtag");
+
+    TCanvas c;
+    c.Divide(2, 1);
+    c.cd(1);
+    h1->Draw();
+    c.cd(2);
+    h2->Draw();
+    c.SaveAs("6_rdataframe_compiled.png");
+}
+
+int main() {
+  TStopwatch s;
+  s.Start();
+  rdataframe();
+  s.Stop();
+  std::cout << "Elapsed: " << s.RealTime() << std::endl;
+}
---
+++ rdf_benchmarks/odb7.cpp
@@ -0,0 +1,71 @@
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RVec.hxx"
+#include "TCanvas.h"
+
+#include <iostream>
+#include "TStopwatch.h"
+
+//#include <ROOT/RLogger.hxx>
+
+//auto verbosity = ROOT::Experimental::RLogScopedVerbosity(ROOT::Detail::RDF::RDFLogChannel(), ROOT::Experimental::ELogLevel::kInfo);
+
+template <typename T> using Vec = const ROOT::RVec<T>&;
+
+ROOT::RVec<int> find_isolated_jets(Vec<float> eta1, Vec<float> phi1, Vec<float> pt2, Vec<float> eta2, Vec<float> phi2)
+{
+    ROOT::RVec<int> mask(eta1.size(), 1);
+    if (eta2.size() == 0) {
+        return mask;
+    }
+
+    const auto ptcut = pt2 > 10;
+    const auto eta2_ptcut = eta2[ptcut];
+    const auto phi2_ptcut = phi2[ptcut];
+    if (eta2_ptcut.size() == 0) {
+        return mask;
+    }
+
+    const auto c = ROOT::VecOps::Combinations(eta1, eta2_ptcut);
+    for (auto i = 0u; i < c[0].size(); i++) {
+        const auto i1 = c[0][i];
+        const auto i2 = c[1][i];
+        const auto dr = ROOT::VecOps::DeltaR(eta1[i1], eta2_ptcut[i2], phi1[i1], phi2_ptcut[i2]);
+        if (dr < 0.4) mask[i1] = 0;
+    }
+    return mask;
+}
+
+void rdataframe() {
+    ROOT::EnableImplicitMT();
+    ROOT::RDataFrame df("Events", "root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root");
+    auto h = df.Filter([](unsigned int n) { return n > 0; }, {"nJet"}, "At least one jet")
+               .Define("goodJet_ptcut", [](const ROOT::RVec<float>& pt) { return pt > 30; }, {"Jet_pt"})
+               .Define("goodJet_antiMuon",
+                       find_isolated_jets,
+                       {"Jet_eta", "Jet_phi", "Muon_pt", "Muon_eta", "Muon_phi"})
+               .Define("goodJet_antiElectron",
+                       find_isolated_jets,
+                       {"Jet_eta", "Jet_phi", "Electron_pt", "Electron_eta", "Electron_phi"})
+               .Define("goodJet",
+                       [](const ROOT::RVec<int> &pt, const ROOT::RVec<int> &muon, const ROOT::RVec<int> &electron) {
+                            return pt && muon && electron;
+                       },
+                       {"goodJet_ptcut", "goodJet_antiMuon", "goodJet_antiElectron"})
+               .Filter([](const ROOT::RVec<int> &good) { return Sum(good) > 0; }, {"goodJet"})
+               .Define("goodJet_sumPt",
+                       [](const ROOT::RVec<int> &good, const ROOT::RVec<float> &pt) { return Sum(pt[good]); },
+                       {"goodJet", "Jet_pt"})
+               .Histo1D<float>({"", ";Jet p_{T} sum (GeV);N_{Events}", 100, 15, 200}, "goodJet_sumPt");
+
+    TCanvas c;
+    h->Draw();
+    c.SaveAs("7_rdataframe_compiled.png");
+}
+
+int main() {
+  TStopwatch s;
+  s.Start();
+  rdataframe();
+  s.Stop();
+  std::cout << "Elapsed: " << s.RealTime() << std::endl;
+}
